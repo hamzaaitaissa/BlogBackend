c70cfec1579b121859c3dcf90a56957cf7e55851
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-02-24 10:11:59 +0000

    Dev: back at it in the comment section

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,9 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using blogfolio.Dto.Comment;
+using blogfolio.Entities;
+using blogfolio.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace blogfolio.Controllers
 {
     public class CommentController : ControllerBase
     {
-       
+        private readonly ICommentService _commentService;
+        private readonly IMapper _mapper;
+
+        public CommentController(ICommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult<Comment>> CreateComment([FromBody] CreateCommentDto createCommentDto)
+        {
+            var comment = await _commentService.CreateCommentAsync(createCommentDto);
+            return CreatedAtAction(nameof(GetComment), new { id = comment.Id }, comment);
+        }
+        public async Task<ActionResult<Task>> GetComment(int id)
+        {
+            var comment = await _commentService.GetCommentAsync(id);
+            if(comment == null)
+            {
+                return NotFound();
+            }
+            return Ok(comment);
+        }
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
+        {
+            var comments = await _commentService.GetAllCommentAsync();
+            return Ok(comments);
+        }
+        public async Task<ActionResult> DeleteComment(int id)
+        {
+            await _commentService.DeleteCommentAsync(id);
+            return Ok();
+        }
     }
 }
