107b22bebedb88a2d827f2b86d9a4281a50eac9e
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-03-05 15:30:34 +0000

    dev: more User logic

---
+++ Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using blogfolio.Dto.User;
+using blogfolio.Entities;
+using blogfolio.Repositories;
+
+namespace blogfolio.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<User> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(UpdateUserDto updateUserDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
