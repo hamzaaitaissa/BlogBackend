b35894e93e3a9164e796c6c90370c88dcb8c403c
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-03-05 15:12:29 +0000

    dev: User repository created and developped

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using blogfolio.Data;
+using blogfolio.Entities;
+
+namespace blogfolio.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<User> AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using blogfolio.Data;
+using blogfolio.Entities;
+
+namespace blogfolio.Repositories
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly BlogfolioContext _blogfolioContext;
+
+        public UserRepository(BlogfolioContext blogfolioContext)
+        {
+            _blogfolioContext = blogfolioContext;
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            await _blogfolioContext.Users.AddAsync(user);
+            await _blogfolioContext.SaveChangesAsync();
+
+            return user;
+
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _blogfolioContext.Users.FindAsync(id);
+            if (user != null)
+            {
+                _blogfolioContext.Users.Remove(user);
+                await _blogfolioContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var user = await _blogfolioContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                throw new KeyNotFoundException($"User with ID {id} not found.");
+            }
+            return user;
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _blogfolioContext.Users.Update(user);
+            await _blogfolioContext.SaveChangesAsync();
+        }
+    }
+}
